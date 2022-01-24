13b91e5bc6cf901168b78c3494dc2406e21df823
sahil <sahilshaikh2286@gmail.com>
2022-01-25 00:48:16 +0530

    Number Pattern

---
+++ rectanglePattern.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // cout<<"hello world";
+    int rows, columns;
+    cout << "enter the numbers of rows and columns";
+    cin >> rows >> columns;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            if (i == 0 || i == rows-1 || j == 0 || j == columns-1)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}

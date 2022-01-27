5e66d152081959733581ef3182fe57474b4c949f
sahil <sahilshaikh2286@gmail.com>
2022-01-27 19:24:03 +0530

    struct in cpp

---
+++ Templates and STL/struct.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct employee{
+    int age;
+    double salary;
+
+    employee(int age_, double salary_){
+        age = age_;
+        salary = salary_;
+    }
+};           
+            
+int main(){
+        
+        // struct is a userDefined dataType which is used to wrapped different dataTypes inside it. 
+        employee sahil(34,455.44);  //new instance of struct employee has been created named as sahil.
+        
+    
+return 0;
+}

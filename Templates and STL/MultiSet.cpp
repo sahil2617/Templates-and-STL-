3b101b8dadec243acda9af7da524e6b0e7ec4456
sahil <sahilshaikh2286@gmail.com>
2022-01-29 20:03:33 +0530

    MultiSet

--- Templates and STL/MultiSet.cpp
+++ Templates and STL/MultiSet.cpp
@@ -1,24 +1,21 @@
-// Below the function given are for ordered set the same follows with the unordered Set 
-    // set/unorderedSet stores unique elements into them . i.e even if users puts an element more than 1 time they will have only one instance of that element 
-//    orderedSet generally keeps element in ascending order and the TimeComplexity of any function performed on it is      O(log(n))..
-//    UnorderedSet generally keeps element in any order and the TimeComplexity of any function performed on it is O(1)..
+// Difference Between the multiSet and Set/unorderedSet is that multiset doesnt store unique elements and set/unorderedset stores unique elements
 
-
+// MultiSet also stores the elements in the ascending order....
+// There is no concept of unordered MultiSets
 
 
 #include <bits/stdc++.h>
 using namespace std;
 
 template <class T>
-// void insert(set<T> &mySet) // this is for set
-void insert(unordered_set<T> &mySet) // this is for unordered set
+void insert(multiset<T> &mySet) 
 {
-    // Inserting an element to the Set: 
-        // a)   mySet = {1,23,4,23,44,23,2};// this is the method of initializing the set..
+    // Inserting an element to the multiSet: 
+        // a)   mySet = {1,23,4,23,44,23,2};// this is the method of initializing the multiset..
 
-        // b) manual insertion of element in the Set
+        // b) manual insertion of element in the multiSet
                 int n;
-                cout << "Enter no. of Elements to be entered in the Set";
+                cout << "Enter no. of Elements to be entered in the multiSet";
                 cin >> n;
                 for (int i = 0; i < n; i++)
                 {
@@ -30,10 +27,9 @@ void insert(unordered_set<T> &mySet) // this is for unordered set
                 }
 }
 template <class T>
-// void display(set<T> &mySet) // this is for set
-void display(unordered_set<T> &mySet) //this is for unordered set
+void display(multiset<T> &mySet) 
 {
-    // Set can be displayed in this way...
+    // multiSet can be displayed in this way...
         for (auto it : mySet)
         {
             cout << it << " ";
@@ -43,12 +39,11 @@ void display(unordered_set<T> &mySet) //this is for unordered set
 
 int main()
 {
-    // set<int> st; //This is for ordered Set.
-    unordered_set<int> st; // This is for unordered set
+    multiset<int> st; 
     insert(st);
     display(st);
 
-    // Erase functionality in Sets
+    // Erase functionality in multiSets
 
             // // 1. st.erase(iterator) // erase method can take an iterator and the element where the iterator is pointing will be erased./..
             // st.erase(st.begin()); // st.begin() points towards the very first element of the set st, so the first element will be deleted
@@ -59,11 +54,11 @@ int main()
             // display(st);
 
             // // 3. st.erase(key)
-            // st.erase(3); // this erase method also takes the key to be erased. Key in the sense any specific element of the set, if that element is present in the set it will be erased.
+            // st.erase(3); // this erase method also takes the key to be erased. Key in the sense any specific element of the set, if that element is present in the multiset all the instances of it will be erased.
             // display(st);
 
     // Finding the element in the set.. 
-            // auto it = st.find(6); ///st.find(element) method returns an iterator to the element...and if the element is not found the iterator is returned to the st.end();
+            // auto it = st.find(6); ///st.find(element) method returns an iterator to the very first occurence of that element...and if the element is not found the iterator is returned to the st.end();
             // st.erase(it);   
             // display(st);
 
